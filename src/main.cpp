e46bb3dea29d2b14fab46a57f57b7811d97828ed
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2022-06-25 10:27:33 +0300

    added hidapi lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <cstdio>
+#include "hidapi.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    struct hid_device_info *devs, *cur_dev;
+
+    devs = hid_enumerate(0x0, 0x0);
+    cur_dev = devs;
+    while (cur_dev) {
+        printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
+               cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+        printf("\n");
+        printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+        printf("  Product:      %ls\n", cur_dev->product_string);
+        printf("\n");
+        cur_dev = cur_dev->next;
+    }
+    hid_free_enumeration(devs);
     return 0;
 }
